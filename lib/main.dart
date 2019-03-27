6836185bf0aed605116f5219cb5923ec12c872bd
ptlCoder <1130354919@qq.com>
2019-03-27 17:05:36 +0800

    修改

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,8 @@ class MyApp extends StatelessWidget {
       title: '微信',
       theme: ThemeData(
 //        primarySwatch: Color(0xff303030),
+
+
       primaryColor: Color(0xff303030),
       ),
       home: TabNavigationBar(),
